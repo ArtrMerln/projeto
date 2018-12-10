a563e48fd64c430ff77d1060d211eb8153b88628
3049255 <3049255@MC113622.ifrn.local>
2018-12-10 16:11:09 -0300

    adição do maven e config de springs

---
+++ src/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String homeSeiLa() {
+		System.out.println("Chamou o método da home!");
+		return "home";
+	}
+	
+}
